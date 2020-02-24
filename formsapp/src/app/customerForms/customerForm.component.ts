1430074166fffc57a77c743f398956f7e5d7187c
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-24 18:58:22 +0530

    validation

--- formsapp/src/app/customerForms/customerForm.component.ts
+++ formsapp/src/app/customerForms/customerForm.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ICustomer} from '../modals/customer.model';
 
 @Component({
 selector:'app-form',
@@ -6,5 +7,15 @@ templateUrl:'./customerFrom.component.html'
 })
 
 export class CustomerFormComponent{
+  maxdays:any[]=['1-day','2-day','3-day'];
+  mycust=new ICustomer('John','','','',true,'','');
+  firstToUpper(value):void{
+    if(value.length> 0){
+      this.mycust.firstname=value.charAt(0).toUpperCase()+value.slice(1);
 
+    }
+    else{
+      this.mycust.firstname=value;
+    }
+  }
 }
---
+++ formsapp/src/app/modals/customer.model.ts
@@ -0,0 +1,9 @@
+export class ICustomer{
+  constructor(
+   public firstname:string,public lastname:string,public emailid:string ,public password:string
+    ,public prime:boolean,public gender:string
+   ,public maxday:string
+  ){
+
+  }
+}
