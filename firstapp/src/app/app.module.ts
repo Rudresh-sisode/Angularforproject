43775694717d3fe3adb11f5cc6d0606dabe388ac
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-24 09:20:41 +0530

    steps are working on the way

--- firstapp/src/app/app.module.ts
+++ firstapp/src/app/app.module.ts
@@ -2,20 +2,19 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import { HeaderComponent } from './header.component';
-
-
+import {RouterModule} from '@angular/router';
+import { ProductComponent } from './products/product.component';
+import { MyUpperPipe } from './products/myupper.pipe';
+import { DiscountPipe } from './products/discount.pipe';
 import {FormsModule} from '@angular/forms';
-
-
+import { ProductSearchPipe } from './products/productFilter.pipe';
+import { StartComponent } from './shared/star.component';
 import { ProductServicev } from './products/product.service';
 import {HttpClientModule} from '@angular/common/http';
 import { OrderComponent } from './order/order.component';
 import { HomeComponent } from './home/home.component';
-
+import { ProductDetailsComponent } from './products/product-detail.component';
 import { NotFoundComponent } from './shared/NotFound.component';
-import { ProductModule } from './products/product.module';
-
-
 
 
 @NgModule({
@@ -24,9 +23,14 @@ import { ProductModule } from './products/product.module';
         BrowserModule,
         FormsModule,
         HttpClientModule,
-
-        ProductModule,
-
+        RouterModule.forRoot([
+          {path:'product',component:ProductComponent},
+          {path:'product/:id',component:ProductDetailsComponent},
+          {path:'orders',component:OrderComponent},
+          {path:'home',component:HomeComponent},
+          {path:'',redirectTo:'home',pathMatch:'full'},
+          {path:'**',component:NotFoundComponent}
+        ])
 
 
     ],
@@ -34,8 +38,11 @@ import { ProductModule } from './products/product.module';
     declarations:[
         AppComponent,
         HeaderComponent,
+
+        StartComponent,
         OrderComponent,
         HomeComponent,
+
         NotFoundComponent
 
 
--- firstapp/src/app/products/product.module.ts
+++ firstapp/src/app/products/product.module.ts
@@ -1,5 +1,7 @@
 import {NgModule} from '@angular/core';
-
+import {CommonModule} from '@angular/common';
+import {FormsModule}from '@angular/forms';
+import {RouterModule} from '@angular/router';
 import { ProductComponent } from './product.component';
 import { MyUpperPipe } from './myupper.pipe';
 import { DiscountPipe } from './discount.pipe';
@@ -7,20 +9,20 @@ import { ProductSearchPipe } from './productFilter.pipe';
 import { ProductDetailsComponent } from './product-detail.component';
 import { ProductServicev } from './product.service';
 
-import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   imports:[
-    SharedModule,
-
-
+    CommonModule,
+    FormsModule,
+    RouterModule
   ],
   declarations:[
-  ProductComponent,
-        MyUpperPipe,
-        DiscountPipe,
-        ProductSearchPipe,
-        ProductDetailsComponent,
+    ProductComponent,
+    MyUpperPipe,
+    DiscountPipe,
+    ProductSearchPipe,
+    ProductDetailsComponent
 
   ],
   providers:[
